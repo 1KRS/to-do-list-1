95172608e0bb6ce614c0c341b2e72fae11af91d9
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-10-20 17:20:32 +0200

    Update index.js (Routing)

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+app.get('/personal', (req, res) => {
+  res.render('personal.ejs')
+})
+app.get('/business', (req, res) => {
+  res.render('business.ejs')
+})
+
+
+
 app.listen(port, () => {
-  console.log(`Διακομιστής: Ενεργός [${port}]`);
+  console.log(`Ο διακομιστής ακούει στην πύλη ${port}`);
 });
