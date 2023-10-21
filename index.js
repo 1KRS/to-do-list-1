f05f81584837a4f2119d44a6f51bc9ebbbe0209b
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-10-21 20:22:42 +0200

    Update index.js
    Create /submit routing
    Creation of to-do arrays

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ app.use(express.static('./public'))
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
+app.get('/submit', (req, res) => {
+  res.render('index.ejs')
+})
 app.get('/personal', (req, res) => {
-  res.render('personal.ejs')
+  res.render('personal.ejs', {personal})
 })
 app.get('/business', (req, res) => {
-  res.render('business.ejs')
+  res.render('business.ejs', {business})
 })
-
-
 
 app.listen(port, () => {
   console.log(`Ο διακομιστής ακούει στην πύλη ${port}`);
 });
+
+
+const personal = [
+  
+]
+const business = [
+
+]
